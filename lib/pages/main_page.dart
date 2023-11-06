b9834cbe02df215c821823736aaf08f67785a43a
Julius <julius.brehme@googlemail.com>
2023-11-06 23:33:57 +0900

    made images same size

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -14,28 +14,48 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 100),
-      child: PageView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          _AlgorithmTile(
-            image: "lib/images/pathfinding.png",
-            description: "Pathfinding",
-            onTap: toPathFinding,
+    return Column(
+      children: [
+        // const SizedBox(
+        //   height: 50,
+        // ),
+        // const Center(
+        //   child: Text(
+        //     "Select Algorithm:",
+        //     style: TextStyle(
+        //       color: Color.fromRGBO(255, 255, 255, 1),
+        //       fontWeight: FontWeight.w500,
+        //       fontFamily: "Outfit",
+        //       fontSize: 30,
+        //     ),
+        //   ),
+        // ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 100),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _AlgorithmTile(
+                  image: "lib/images/pathfinding.png",
+                  description: "Pathfinding",
+                  onTap: toPathFinding,
+                ),
+                _AlgorithmTile(
+                  image: "lib/images/sorting.png",
+                  description: "Sorting",
+                  onTap: toSorting,
+                ),
+                _AlgorithmTile(
+                  image: "lib/images/treesearch.png",
+                  description: "Treesearch",
+                  onTap: toTreeSearch,
+                ),
+              ],
+            ),
           ),
-          _AlgorithmTile(
-            image: "lib/images/sorting.png",
-            description: "Sorting",
-            onTap: toSorting,
-          ),
-          _AlgorithmTile(
-            image: "lib/images/treesearch.png",
-            description: "Treesearch",
-            onTap: toTreeSearch,
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
@@ -55,41 +75,48 @@ class _AlgorithmTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 50),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: InkWell(
         onTap: onTap,
-        child: Card(
-          elevation: 10,
-          color: const Color.fromRGBO(51, 74, 100, 1),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50),
-          ),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 110,
-              ),
-              Image.asset(image),
-              const SizedBox(
-                height: 60,
-              ),
-              Text(
-                description,
-                style: const TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Outfit",
-                    fontSize: 30),
-              ),
-              const Text(
-                "Visualization",
-                style: TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Outfit",
-                    fontSize: 30),
-              ),
-            ],
+        child: SizedBox(
+          width: 300,
+          child: Card(
+            elevation: 10,
+            color: const Color.fromRGBO(51, 74, 100, 1),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 110,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset(image),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  description,
+                  style: const TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Outfit",
+                      fontSize: 30),
+                ),
+                const Text(
+                  "Visualization",
+                  style: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Outfit",
+                      fontSize: 30),
+                ),
+              ],
+            ),
           ),
         ),
       ),
