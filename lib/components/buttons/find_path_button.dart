79e89785ab5fa1c3aee1f1870db2f1ee15892742
Julius <julius.brehme@googlemail.com>
2023-11-09 18:51:54 +0900

    updated button to be able to use dfs

--- lib/components/buttons/find_path_button.dart
+++ lib/components/buttons/find_path_button.dart
@@ -1,42 +1,75 @@
-import 'package:algo_verse_app/components/visualization_fab/algorithm_buttons.dart';
-import 'package:algo_verse_app/components/visualization_fab/visualize_button.dart';
+import 'package:algo_verse_app/components/buttons/visualization_fab/algorithm_buttons.dart';
+import 'package:algo_verse_app/components/buttons/visualization_fab/visualize_button.dart';
+import 'package:algo_verse_app/components/path_finding/algorithms/dfs.dart';
+import 'package:algo_verse_app/components/path_finding/pathfinding_coordinator.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FindPathButton extends StatelessWidget {
   const FindPathButton({super.key});
 
+// das wird zum consumer oder so, der dann auf pathfindingcontroller zu greifen kann. dann wird hier
+// mit den sachen auch pathfindingcoordintor die entsprechende funktion aufgerufen und dann coordinator
+// geupdatet -> dadurch werden alle consumer geupdatet, also sollte sich die page updaten
 
   @override
   Widget build(BuildContext context) {
-    return VisualizeButton(
-      distance: 80,
-      children: [
-        AlgorithmButton(
-          onPressed: () {
-            print("Dijkstra");
-          },
-          algorithm: "Dijkstra",
-        ),
-        AlgorithmButton(
-          onPressed: () {
-            print("DFS");
-          },
-          algorithm: "DFS",
-          //closeFabOnTap: true,
-        ),
-        AlgorithmButton(
-          onPressed: () {
-            print("BFS");
-          },
-          algorithm: "BFS",
-        ),
-        AlgorithmButton(
-          onPressed: () {
-            print("Astar");
-          },
-          algorithm: "Astar",
-        ),
-      ],
+    return Consumer<PathFindingCoordinator>(
+      builder: (context, coordinator, child) => VisualizeButton(
+        distance: 80,
+        children: [
+          AlgorithmButton(
+            onPressed: () {
+              // call dijkstra and update path and visited -> it will be printed to screen
+              List<Node> nodes = [
+                Node(location: Location(0, 0)),
+                Node(location: Location(1, 1)),
+                Node(location: Location(1, 2)),
+                Node(location: Location(2, 2)),
+                Node(location: Location(3, 2)),
+                Node(location: Location(4, 2)),
+                Node(location: Location(5, 2)),
+                Node(location: Location(6, 2)),
+                Node(location: Location(7, 2)),
+                Node(location: Location(8, 2)),
+                Node(location: Location(9, 2)),
+                Node(location: Location(10, 2)),
+              ];
+              coordinator.addAllObstacle(nodes);
+            },
+            algorithm: "Dijkstra",
+          ),
+          AlgorithmButton(
+            onPressed: () {
+              DFS dfs = DFS(
+                startingNode: coordinator.getStartNode,
+                endingNode: coordinator.getEndingNode,
+                obstacles: coordinator.getAllObstacles,
+                boardSize: coordinator.getSize,
+              );
+              List<List<Node>> pathInformation = dfs.findPath();
+              coordinator.addVisitedNodes(pathInformation.last);
+              coordinator.addPath(pathInformation.first);
+            },
+            algorithm: "DFS",
+            //closeFabOnTap: true,
+          ),
+          AlgorithmButton(
+            onPressed: () {
+              print("BFS");
+            },
+            algorithm: "BFS",
+          ),
+          AlgorithmButton(
+            onPressed: () {
+              print("Astar");
+            },
+            algorithm: "Astar",
+          ),
+        ],
+      ),
     );
   }
 }
+
+class Nodes {}
