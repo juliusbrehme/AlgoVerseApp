17498d7af0fd4792217f6c5581da924c8bec0c5b
Julius <julius.brehme@googlemail.com>
2023-11-30 16:12:08 +0900

    updated size of button

--- lib/components/buttons/action_button.dart
+++ lib/components/buttons/action_button.dart
@@ -4,12 +4,14 @@ class ActionButton extends StatelessWidget {
   const ActionButton({
     super.key,
     required this.height,
+    required this.width,
     required this.radius,
     required this.onTap,
     required this.icon,
   });
 
   final double height;
+  final double width;
   final double radius;
   final VoidCallback onTap;
   final Widget icon;
@@ -18,14 +20,17 @@ class ActionButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: height,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromRGBO(51, 74, 100, 1),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(radius))),
+      child: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(70, 40),
+            backgroundColor: const Color.fromRGBO(51, 74, 100, 1),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(radius))),
+          ),
+          onPressed: onTap,
+          child: icon,
         ),
-        onPressed: onTap,
-        child: icon,
       ),
     );
   }
