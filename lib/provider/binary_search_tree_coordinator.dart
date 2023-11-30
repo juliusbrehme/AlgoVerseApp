a78afcc41cc6bb38c76f8e5d8edccfc3255392c4
Julius <julius.brehme@googlemail.com>
2023-11-30 19:53:35 +0900

    changed constructor for tree coordinator

--- lib/provider/binary_search_tree_coordinator.dart
+++ lib/provider/binary_search_tree_coordinator.dart
@@ -1,16 +1,12 @@
-import 'dart:math';
-
 import 'package:algo_verse_app/algorithms/binary_search_tree/binary_search_tree.dart';
 import 'package:algo_verse_app/algorithms/binary_search_tree/node.dart';
 import 'package:flutter/material.dart';
 
 class BinarySearchTreeCoordinator extends ChangeNotifier {
-  BinarySearchTree _binaryTree = BinarySearchTree();
+  final BinarySearchTree _binaryTree;
 
-  BinarySearchTreeCoordinator() {
-    _binaryTree = BinarySearchTree.fromList(
-        List.generate(15, (index) => Random().nextInt(100)));
-    binaryTree.calculateNodePositions(binaryTree.root);
+  BinarySearchTreeCoordinator(this._binaryTree) {
+    binaryTree.calculateNodePositions(_binaryTree.root);
   }
 
   BinarySearchTree get binaryTree => _binaryTree;
