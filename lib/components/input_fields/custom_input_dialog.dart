7c7dcdbdf74db42d0e0d9a06abd83478e50d760d
Julius <julius.brehme@googlemail.com>
2023-12-05 15:57:10 +0900

    can also add a number by pressing done on the keyboard

--- lib/components/input_fields/custom_input_dialog.dart
+++ lib/components/input_fields/custom_input_dialog.dart
@@ -108,6 +108,9 @@ class _CustomInputDialogState extends State<CustomInputDialog> {
                           ),
                         ),
                       ],
+                      onEditingComplete: () {
+                        addNumber();
+                      },
                     ),
                   ),
                   TextButton(
