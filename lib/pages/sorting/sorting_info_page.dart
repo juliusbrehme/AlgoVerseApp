4f557b39adf28656a42ec0bcf00eacfe6f8fa998
Julius <julius.brehme@googlemail.com>
2023-12-13 16:15:45 +0900

    refactoring

---
+++ lib/pages/sorting/sorting_info_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SortingInfoPage extends StatelessWidget {
+  const SortingInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/pages/treesearch/treesearch_info_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TreeSearchInfoPage extends StatelessWidget {
+  const TreeSearchInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
