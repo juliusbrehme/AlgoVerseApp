c1c8aad030a6700f5f614d872e505f51f871ad60
Julius <julius.brehme@googlemail.com>
2023-12-14 23:49:44 +0900

    added the detailed path

--- lib/components/information/path_history_block.dart
+++ lib/components/information/path_history_block.dart
@@ -1,7 +1,8 @@
+import 'package:algo_verse_app/algorithms/path_finding/node.dart';
 import 'package:algo_verse_app/algorithms/path_finding/path_history.dart';
 import 'package:flutter/material.dart';
 
-class PathHistoryBlock extends StatelessWidget {
+class PathHistoryBlock extends StatefulWidget {
   const PathHistoryBlock({
     super.key,
     required this.pathHistory,
@@ -10,12 +11,52 @@ class PathHistoryBlock extends StatelessWidget {
   final PathHistory pathHistory;
 
   @override
+  State<PathHistoryBlock> createState() => _PathHistoryBlockState();
+}
+
+class _PathHistoryBlockState extends State<PathHistoryBlock> {
+  bool expanded = false;
+
+  Widget showNode(int index, Node node) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, bottom: 8),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 79, 115, 156),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RichText(
+            text: TextSpan(
+              text: '${(index + 1).toString()}: ',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              children: <TextSpan>[
+                TextSpan(
+                    text: "(${node.x}, ${node.y})",
+                    style: TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget showTrack(PathHistory pathHistory) {
+    return Wrap(
+      children: List.generate(pathHistory.visitedNodes.length,
+          (index) => showNode(index, pathHistory.visitedNodes[index])),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
       child: Container(
         width: MediaQuery.of(context).size.width - 50,
-        height: 100,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           color: const Color.fromRGBO(51, 74, 100, 1),
@@ -25,13 +66,13 @@ class PathHistoryBlock extends StatelessWidget {
             left: 20,
             top: 10,
             right: 20,
-            //bottom: 10,
+            bottom: 20,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                pathHistory.algorithm,
+                widget.pathHistory.algorithm,
                 style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
@@ -53,7 +94,7 @@ class PathHistoryBlock extends StatelessWidget {
                             color: const Color.fromRGBO(245, 245, 245, 1)),
                       ),
                       Text(
-                        pathHistory.startingNode.toString(),
+                        widget.pathHistory.startingNode.toString(),
                         style: TextStyle(
                             color: const Color.fromRGBO(245, 245, 245, 1)),
                       )
@@ -67,7 +108,7 @@ class PathHistoryBlock extends StatelessWidget {
                             color: const Color.fromRGBO(245, 245, 245, 1)),
                       ),
                       Text(
-                        pathHistory.targetNode.toString(),
+                        widget.pathHistory.targetNode.toString(),
                         style: TextStyle(
                             color: const Color.fromRGBO(245, 245, 245, 1)),
                       )
@@ -81,7 +122,7 @@ class PathHistoryBlock extends StatelessWidget {
                             color: const Color.fromRGBO(245, 245, 245, 1)),
                       ),
                       Text(
-                        pathHistory.numberOfVisitedNodes.toString(),
+                        widget.pathHistory.numberOfVisitedNodes.toString(),
                         style: TextStyle(
                             color: const Color.fromRGBO(245, 245, 245, 1)),
                       )
@@ -90,20 +131,36 @@ class PathHistoryBlock extends StatelessWidget {
                   Container(
                     height: 40,
                     width: 80,
-                    child: Center(
-                        child: Text(
-                      "See Track",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    )),
                     decoration: BoxDecoration(
-                      color: Color.fromRGBO(75, 107, 157, 1),
+                      color: expanded
+                          ? Colors.red
+                          : Color.fromRGBO(75, 107, 157, 1),
                       borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            expanded = !expanded;
+                          });
+                        },
+                        child: Text(
+                          expanded ? "Hide" : "See Track",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
+              expanded
+                  ? Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: showTrack(widget.pathHistory))
+                  : SizedBox(),
             ],
           ),
         ),
